8e740e2cd9db3270790770305d19ab6ccedbe444
Juan Caicedo <juan@spanishdict.com>
2015-11-22 17:38:05 -0500

    add npm tasks

--- build.js
+++ build.js
@@ -1,10 +1,14 @@
 var Metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
+var serve = require('metalsmith-serve');
 
 Metalsmith(__dirname)
   .source('./src')
   .destination('./build')
   .use(markdown())
+  .use(serve({}))
   .build(function(err) {
-    if (err) console.log(err)
+    if (err) {
+      console.log(err)
+    }
   })
